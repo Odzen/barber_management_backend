eabf304ec26cc38e1c58880df47f1b1394931aaf
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-04-22 14:46:13 -0500

    comment resolvers to refactor

--- src/auth.ts
+++ src/auth.ts
@@ -1,109 +1,109 @@
-import { TextEncoder } from 'util'
+// import { TextEncoder } from 'util'
 
-import { PrismaClient } from '@prisma/client'
-import { compare } from 'bcrypt'
-import { SignJWT, jwtVerify } from 'jose'
+// import { PrismaClient } from '@prisma/client'
+// import { compare } from 'bcrypt'
+// import { SignJWT, jwtVerify } from 'jose'
 
-import type { Request, RequestHandler } from 'express'
+// import type { Request, RequestHandler } from 'express'
 
-declare global {
-	namespace Express {
-		interface User {
-			id: string
-		}
-		interface Request {
-			user?: Express.User
-		}
-	}
-}
+// declare global {
+// 	namespace Express {
+// 		interface User {
+// 			id: string
+// 		}
+// 		interface Request {
+// 			user?: Express.User
+// 		}
+// 	}
+// }
 
-interface UserJwtPayload {
-	id: string // The user Id
-	iat: number // Issued at
-	exp: number // Expire time
-}
+// interface UserJwtPayload {
+// 	id: string // The user Id
+// 	iat: number // Issued at
+// 	exp: number // Expire time
+// }
 
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'nunca pares de aprender'
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'nunca pares de aprender'
 
-const orm = new PrismaClient()
+// const orm = new PrismaClient()
 
-/**
- * Login with redirect support.
- *
- * The response will be redirected to the given `redirect` value if any.
- */
-export const login: RequestHandler = async (req, res) => {
-	const { username, password } = req.body
+// /**
+//  * Login with redirect support.
+//  *
+//  * The response will be redirected to the given `redirect` value if any.
+//  */
+// export const login: RequestHandler = async (req, res) => {
+// 	const { username, password } = req.body
 
-	try {
-		const user = await orm.user.findUnique({ where: { username: username } })
+// 	try {
+// 		const user = await orm.user.findUnique({ where: { username: username } })
 
-		if (!user) {
-			throw new Error()
-		}
+// 		if (!user) {
+// 			throw new Error()
+// 		}
 
-		const isValid = await compare(password, user.password)
+// 		const isValid = await compare(password, user.password)
 
-		if (!isValid) {
-			throw new Error()
-		}
+// 		if (!isValid) {
+// 			throw new Error()
+// 		}
 
-		const token = await new SignJWT({ id: user.id })
-			.setProtectedHeader({ alg: 'HS256' })
-			.setIssuedAt()
-			.setExpirationTime('2h')
-			.sign(new TextEncoder().encode(JWT_SECRET_KEY))
+// 		const token = await new SignJWT({ id: user.id })
+// 			.setProtectedHeader({ alg: 'HS256' })
+// 			.setIssuedAt()
+// 			.setExpirationTime('2h')
+// 			.sign(new TextEncoder().encode(JWT_SECRET_KEY))
 
-		res.json({ token, username: user.username, id: user.id })
-	} catch (err) {
-		res.sendStatus(401)
-	}
-}
+// 		res.json({ token, username: user.username, id: user.id })
+// 	} catch (err) {
+// 		res.sendStatus(401)
+// 	}
+// }
 
-export const verifyToken = async (req: Request) => {
-	const { authorization } = req.headers
-	const token = (authorization || '').replace('Bearer ', '')
+// export const verifyToken = async (req: Request) => {
+// 	const { authorization } = req.headers
+// 	const token = (authorization || '').replace('Bearer ', '')
 
-	try {
-		const verified = await jwtVerify(
-			token,
-			new TextEncoder().encode(JWT_SECRET_KEY)
-		)
+// 	try {
+// 		const verified = await jwtVerify(
+// 			token,
+// 			new TextEncoder().encode(JWT_SECRET_KEY)
+// 		)
 
-		return verified.payload as unknown as UserJwtPayload
-	} catch (e) {
-		throw new Error('Invalid token')
-	}
-}
+// 		return verified.payload as unknown as UserJwtPayload
+// 	} catch (e) {
+// 		throw new Error('Invalid token')
+// 	}
+// }
 
-const authMiddleware: RequestHandler = async (req, _res, next) => {
-	try {
-		const payload = await verifyToken(req)
-		req.user = { id: payload.id }
-	} catch (e) {
-		// ignore
-	} finally {
-		next()
-	}
-}
+// const authMiddleware: RequestHandler = async (req, _res, next) => {
+// 	try {
+// 		const payload = await verifyToken(req)
+// 		req.user = { id: payload.id }
+// 	} catch (e) {
+// 		// ignore
+// 	} finally {
+// 		next()
+// 	}
+// }
 
-export default authMiddleware
+// export default authMiddleware
 
-export const currentUser: RequestHandler = async (req, res) => {
-	try {
-		const userDetails = await orm.user.findUnique({
-			where: { id: req.user?.id },
-		})
+// export const currentUser: RequestHandler = async (req, res) => {
+// 	try {
+// 		const userDetails = await orm.user.findUnique({
+// 			where: { id: req.user?.id },
+// 		})
 
-		if (!userDetails) {
-			throw new Error()
-		}
+// 		if (!userDetails) {
+// 			throw new Error()
+// 		}
 
-		res.json({
-			id: userDetails.id,
-			username: userDetails.username,
-		})
-	} catch (e) {
-		res.sendStatus(401)
-	}
-}
+// 		res.json({
+// 			id: userDetails.id,
+// 			username: userDetails.username,
+// 		})
+// 	} catch (e) {
+// 		res.sendStatus(401)
+// 	}
+// }
