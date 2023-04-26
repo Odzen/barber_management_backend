38c19dc8f463cfbc47f86e28ed46e8015b51710e
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-04-26 01:01:32 -0500

    schema updated

---
+++ prisma/migrations/20230426055934_init/migration.sql
@@ -0,0 +1,122 @@
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('ADMIN', 'BARBER', 'CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "STATE" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "APPOINTMENTE_STATE" AS ENUM ('CANCELED', 'SCHEDULED', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "SERVICE" AS ENUM ('HAIRCUT', 'BEARD');
+
+-- CreateTable
+CREATE TABLE "Avocado" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+    "name" TEXT NOT NULL,
+    "sku" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Avocado_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attributes" (
+    "id" TEXT NOT NULL,
+    "description" TEXT,
+    "shape" TEXT,
+    "hardiness" TEXT,
+    "taste" TEXT,
+    "avocadoId" TEXT NOT NULL,
+
+    CONSTRAINT "Attributes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" SERIAL NOT NULL,
+    "name" "SERVICE" NOT NULL DEFAULT 'HAIRCUT',
+    "price" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "documentNumber" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "urlImg" TEXT,
+    "email" TEXT NOT NULL,
+    "state" "STATE" NOT NULL DEFAULT 'ACTIVE',
+    "role" "ROLE" NOT NULL DEFAULT 'CUSTOMER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Engagement" (
+    "administratorId" INTEGER NOT NULL,
+    "barberId" INTEGER NOT NULL,
+    "salary" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Engagement_pkey" PRIMARY KEY ("administratorId","barberId")
+);
+
+-- CreateTable
+CREATE TABLE "Appointment" (
+    "customerId" INTEGER NOT NULL,
+    "barberId" INTEGER NOT NULL,
+    "serviceId" INTEGER NOT NULL,
+    "appointmentDate" TIMESTAMP(3) NOT NULL,
+    "state" "APPOINTMENTE_STATE" NOT NULL DEFAULT 'SCHEDULED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("customerId","barberId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Avocado_sku_key" ON "Avocado"("sku");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Attributes_avocadoId_key" ON "Attributes"("avocadoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_documentNumber_key" ON "User"("documentNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Attributes" ADD CONSTRAINT "Attributes_avocadoId_fkey" FOREIGN KEY ("avocadoId") REFERENCES "Avocado"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Engagement" ADD CONSTRAINT "Engagement_administratorId_fkey" FOREIGN KEY ("administratorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Engagement" ADD CONSTRAINT "Engagement_barberId_fkey" FOREIGN KEY ("barberId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_barberId_fkey" FOREIGN KEY ("barberId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
