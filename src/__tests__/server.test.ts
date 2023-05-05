e775c3a6f6d1abd3dc5d59bd88b060a88a1f8b21
victo-18 <alomia18@gmail.com>
2023-05-05 00:01:08 -0500

    pruebas para el back modulo server

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -4,17 +4,18 @@ import request from "supertest";
 import morgan from "morgan";
 
 describe("Testing the server", () => {
+//probando el inicio del servidor
   test("Server starts successfully", async () => {
     const server = await app.listen(3000);
     expect(server).toBeDefined();
     server.close();
   });
-
+// probando la respuesta de la ruta
   test("GET / responds with 200 status code", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
   });
-
+//verificando uso del modulo morgan
   test("Morgan middleware is being used", async () => {
     const morganSpy = jest.fn();
     app.use(morgan("dev"));
