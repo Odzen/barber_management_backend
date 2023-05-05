4aed5777ea2a1362aae52fb56f71885cc8c5c4b3
alomia18 <alomia18@gmail.com>
2023-05-04 23:34:32 -0500

    probando la conexion del metodo get

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,11 +1,19 @@
-import { expect, describe, test } from '@jest/globals'
-import app from '../server'
-describe('',()=>{
-    test('Server starts successfully', async () => {
-        const server = await app.listen(3000)
-        expect(server).toBeDefined()
-        server.close()
-      })
-})
-    
+import { jest, expect, describe, test } from "@jest/globals";
+import app from "../server";
+import request from "supertest";
+import morgan from "morgan";
 
+describe("Testing the server", () => {
+  test("Server starts successfully", async () => {
+    const server = await app.listen(3000);
+    expect(server).toBeDefined();
+    server.close();
+  });
+
+  test("GET / responds with 200 status code", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+  });
+
+ 
+});
