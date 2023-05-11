3715e8551ff4c397e5e839518afe850ed4970d21
victo-18 <alomia18@gmail.com>
2023-05-10 20:48:21 -0500

    mock para probar la respuesta de la funcion errorHandler que sea en json con un mensaje de error

--- src/__tests__/error-handler.test.ts
+++ src/__tests__/error-handler.test.ts
@@ -1,10 +1,10 @@
 import { expect, it, describe, jest } from '@jest/globals'
-import { Request, Response } from 'express'
+import { Request, Response,NextFunction } from 'express'
 
-import { logErrors } from '../middlewares/error.handler'
-
+import { logErrors ,errorHandler} from '../middlewares/error.handler'
+// probando que la función logErrors imprima el error proporcionado en la consola 
 describe('logErrors middleware', () => {
-	it('debería imprimir el error en la consola y pasar el control al siguiente middleware', () => {
+	it('should print the error to the console and pass control to the next middleware', () => {
 		const err = new Error('Este es un error de prueba')
 		const req = {} as Request
 		const res = {} as Response
@@ -22,5 +22,24 @@ describe('logErrors middleware', () => {
 		expect(next).toHaveBeenCalledWith(err)
 
 		consoleErrorMock.mockRestore()
-	})
-})
+	});
+    //probando errorHandler que su respuesta se a un json con un mensaje de error
+    it('should send a JSON response with the error message, stack and original error', () => {
+        const mockErr = new Error('Test error');
+        const mockReq = {} as Request;
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn().mockReturnThis(),
+        } as unknown as Response;
+        const mockNext = jest.fn() as NextFunction;
+    
+        errorHandler(mockErr, mockReq, mockRes, mockNext);
+    
+        expect(mockRes.status).toHaveBeenCalledWith(500);
+        expect(mockRes.json).toHaveBeenCalledWith({
+          message: 'Test error',
+          stack: mockErr.stack,
+          err: mockErr,
+        });
+      });
+});
