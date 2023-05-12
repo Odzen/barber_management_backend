61aec9adafe93eed3cdd9584eeba328e29463ab0
Odzen <jsebastian.va@gmail.com>
2023-05-12 00:54:21 -0500

    Set

--- src/__tests__/example2.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, describe, test } from '@jest/globals'
-
-// Example test
-describe('Wrong test', () => {
-	test('should pass', () => {
-		expect(3).toEqual(3)
-	})
-})
---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,28 @@
+import { jest, expect, describe, test } from '@jest/globals'
+import morgan from 'morgan'
+import request from 'supertest'
+
+import app from '../server'
+
+describe('Testing the server', () => {
+	//const port = parseInt(process.env.PORT) || 80
+	//probando el inicio del servidor
+	test('Server starts successfully', async () => {
+		//console.log(port)
+		const server = await app.listen(3000)
+		expect(server).toBeDefined()
+		server.close()
+	})
+	// probando la respuesta de la ruta
+	test('GET / responds with 200 status code', async () => {
+		const res = await request(app).get('/')
+		expect(res.status).toBe(200)
+	})
+	//verificando uso del modulo morgan
+	test('Morgan middleware is being used', async () => {
+		const morganSpy = jest.fn()
+		app.use(morgan('dev'))
+		await request(app).get('/')
+		expect(morganSpy.mock.calls.length > 0).toBe(false)
+	})
+})
