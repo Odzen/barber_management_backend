cd7ac7746be2cc67411d135fca40efea40696551
victo-18 <alomia18@gmail.com>
2023-05-24 01:28:46 -0500

    pruebas adicionales para el enrutador en local strategys y sus respuestas

--- src/__tests__/strategies/local.strategy.test.ts
+++ src/__tests__/strategies/local.strategy.test.ts
@@ -9,7 +9,9 @@ import {
 } from '@jest/globals'
 import { compare } from 'bcrypt'
 import { IVerifyOptions, Strategy as LocalStrategy } from 'passport-local'
+import supertest from 'supertest'
 
+import router from '../../routes/auth.router'
 import app from '../../server'
 import UserService from '../../services/users.service'
 
@@ -100,4 +102,32 @@ describe('localStrategy', () => {
 			expect(done).toHaveBeenCalledWith(Error, false)
 		})
 	})
-});
+	// Prueba de error al buscar el usuario por correo electrónico
+	test('should return error if an error occurs when searching for user by email', async () => {
+		const email = 'testbarbery.com'
+		userService.findByEmail(email)
+		const response = await supertest(app)
+			.post('/login')
+			.send({ email })
+		expect(userService.findByEmail).toHaveBeenCalledWith(email)
+		expect(response.status).toBe(404)
+	})
+	// Prueba de respuesta JSON en el manejo de errores
+	test('should return JSON error response when authentication error occurs', async () => {
+		const email = 'test@example.com'
+		const error = new Error('Something went wrong')
+		userService.findByEmail(email)
+		const done = jest.fn().mockImplementation(() => done(error, false))
+		const response = await supertest(app).post('/login').send({ email })
+		expect(userService.findByEmail).toHaveBeenCalledWith(email)
+		expect(response.status).toBe(404)	
+	})
+
+	// Prueba de configuración del enrutador
+	test('should configure local authentication strategy in the router', () => {
+		expect(router.stack).toHaveLength(1) // Verifica que solo haya una ruta configurada en el enrutador
+		expect(router.stack[0].route.path).toBe('/login') // Verifica que la ruta sea '/login'
+		expect(router.stack[0].route.methods).toHaveProperty('post') // Verifica que la ruta permita el método POST
+		expect(router.stack[0].route.stack).toHaveLength(2) // Verifica que se hayan configurado dos middlewares para la ruta
+	})
+})
