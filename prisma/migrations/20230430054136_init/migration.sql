c0b7513e4ad532addf49b26e6d500c251ea2aa32
Odzen <jsebastian.va@gmail.com>
2023-05-31 00:31:08 -0500

    changed service enum

--- prisma/migrations/20230430054136_init/migration.sql
+++ prisma/migrations/20230430054136_init/migration.sql
@@ -7,39 +7,10 @@ CREATE TYPE "STATE" AS ENUM ('ACTIVE', 'INACTIVE');
 -- CreateEnum
 CREATE TYPE "APPOINTMENT_STATE" AS ENUM ('CANCELED', 'SCHEDULED', 'DONE');
 
--- CreateEnum
-CREATE TYPE "SERVICE" AS ENUM ('HAIRCUT', 'BEARD');
-
--- CreateTable
-CREATE TABLE "Avocado" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-    "deletedAt" TIMESTAMP(3),
-    "name" TEXT NOT NULL,
-    "sku" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "image" TEXT NOT NULL,
-
-    CONSTRAINT "Avocado_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Attributes" (
-    "id" TEXT NOT NULL,
-    "description" TEXT,
-    "shape" TEXT,
-    "hardiness" TEXT,
-    "taste" TEXT,
-    "avocadoId" TEXT NOT NULL,
-
-    CONSTRAINT "Attributes_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateTable
 CREATE TABLE "Service" (
     "id" TEXT NOT NULL,
-    "name" "SERVICE" NOT NULL DEFAULT 'HAIRCUT',
+    "name" TEXT NOT NULL DEFAULT 'HAIRCUT',
     "price" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
@@ -91,12 +62,6 @@ CREATE TABLE "Appointment" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Avocado_sku_key" ON "Avocado"("sku");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Attributes_avocadoId_key" ON "Attributes"("avocadoId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "User_documentNumber_key" ON "User"("documentNumber");
 
 -- CreateIndex
@@ -104,9 +69,6 @@ CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Attributes" ADD CONSTRAINT "Attributes_avocadoId_fkey" FOREIGN KEY ("avocadoId") REFERENCES "Avocado"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Engagement" ADD CONSTRAINT "Engagement_administratorId_fkey" FOREIGN KEY ("administratorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
